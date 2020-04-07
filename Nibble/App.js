97b5d8b479e67fd7fb10bcf08f887f4dc671c1b7
Eric Zhan <ezhan@usc.edu>
2020-04-06 19:21:34 -0700

    minor changes

--- Nibble/App.js
+++ Nibble/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
