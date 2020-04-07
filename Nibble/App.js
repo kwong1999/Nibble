e25933176de00675937c21b81f0459f9b2126556
Eric Zhan <ezhan@usc.edu>
2020-04-06 23:05:14 -0700

    firebase and ui

--- Nibble/App.js
+++ Nibble/App.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-=======
+
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Menu from './Home';
->>>>>>> 9ba454ddc934fe34ee421d1cc0801ce47d8ca21d
 
 const Stack = createStackNavigator();
 
